e535853f285fb5c30cc3cd3c891d6f5e5ca9fb34
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-04-30 23:24:20 +0300

    ю

---
+++ src/GlobalStyled.jsx
@@ -0,0 +1,9 @@
+import {createGlobalStyle} from "style-components"
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Segoe UI', sans-serif;
+    background-color: #fffbe6;
+  }
+`;
