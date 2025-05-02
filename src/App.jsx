421a80a640eea70c184158576e9bd3b488d1d7fc
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-05-02 20:52:38 +0300

    ,

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RecipeItem } from "./components/recipieItem/RecipieItem";
 import { RecipeList } from "./components/recipieList/RecipieList";
 import recipie from "./recipe.json"
 
+
 export const App = () => {
   return <RecipeList  recipies = {recipie} />
 };
